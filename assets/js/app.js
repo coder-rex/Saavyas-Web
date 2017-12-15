1f87d2bdab5f371e6aa4c9a2f2ae55bd4028166f
coder-rex <ramotra1998@gmail.com>
2017-12-15 22:25:20 +0530

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,7 +90,7 @@ particlesJS('particles-js',
         "mode": "bubble"
       },
       "onclick": {
-        "enable": false,
+        "enable": true,
         "mode": "push"
       },
       "resize": true
